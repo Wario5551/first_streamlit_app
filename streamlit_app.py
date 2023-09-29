ede6ffe13c4c519906c9ed68066da834565fc3c1
Luis Medero <110438784+Wario5551@users.noreply.github.com>
2023-09-29 11:17:47 -0400

    Update streamlit_app.py
    Added metadata query

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,19 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-import snowflake.connector
-
 # Normalizes the json version 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Displays normalized table
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
+
